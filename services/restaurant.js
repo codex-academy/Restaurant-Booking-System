e576b6db158b16a4f00d3a413a278e35889799aa
Raadiyah Arend <raadiyaharend6@gmail.com>
2023-09-11 16:44:02 +0200

    pusedo code

--- services/restaurant.js
+++ services/restaurant.js
@@ -1,12 +1,20 @@
 // restaurant.js
 const restaurant = (db) => {
     async function getTables() {
+        // Use db.manyOrNone to fetch all available tables
+        // Return the result
         return await db.manyOrNone(
             "SELECT * FROM table_booking WHERE booked = false"
         );
     }
 
     async function bookTable({ tableName, username, phoneNumber, seats }) {
+        // Use db.oneOrNone to fetch a table by name check 
+
+        // If the table doesn't exist or is already booked, return an error message
+        // update the table status to booked with the provided details
+        // SQL query: UPDATE table_booking SET booked = true, username = $1, number_of_people = $2, contact_number = $3 WHERE table_name = $4 AND booked = false
+        // Return null if booking is successful
         const table = await db.oneOrNone(
             "SELECT * FROM table_booking WHERE table_name = $1 AND booked = false",
             [tableName]
@@ -33,7 +41,9 @@ const restaurant = (db) => {
 
         return null;
     }
-
+    // If the table doesn't exist or is already booked, return an error message
+    //or update the table status to booked with the provided details
+    //Return null if its sucessful
     async function getBookedTables() {
         return await db.any("SELECT * FROM table_booking WHERE booked = true");
     }
@@ -43,11 +53,15 @@ const restaurant = (db) => {
             "SELECT EXISTS (SELECT 1 FROM table_booking WHERE table_name = $1 AND booked = true)",
             [tableName]
         );
+        // Use db.one to check if a table with the given name is booked
+        // Return the boolean result (true if booked, false if not)
         return result.exists;
     }
 
     async function cancelTableBooking(tableName) {
         return await db.none(
+            // Use db.none to cancel a booking for the specified table
+            //no need to return anything
             "UPDATE table_booking " +
             "SET booked = false, username = NULL, number_of_people = NULL, contact_number = NULL " +
             "WHERE table_name = $1 AND booked = true",
@@ -57,15 +71,14 @@ const restaurant = (db) => {
 
     async function getBookedTablesForUser(username) {
         // Use db.any to fetch all bookings for a specific user
-        // SQL query: SELECT * FROM table_booking WHERE username = $1 AND booked = true
         // Return the result
         return await db.any(
             "SELECT * FROM table_booking WHERE username = $1 AND booked = true",
             [username]
         );
     }
-    /// Export the functions as an object
     return {
+        /// Export the functions as an object
         getTables,
         bookTable,
         getBookedTables,
